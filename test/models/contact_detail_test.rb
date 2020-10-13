ba0c158db33acef3ab7ddf0afc539cdd9b678d5b
Chandra Adhikari <chandra.adhikari1994@gmail.com>
2020-10-14 02:10:41 +0530

    Add coverage folder for simple cov and add mailer translations

--- test/models/contact_detail_test.rb
+++ test/models/contact_detail_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class ContactDetailTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save contact_detail without first_name" do
+    contact_detail = ContactDetail.new
+    assert_not contact_detail.save
+  end
+
+  test "should not save contact_detail without last_name" do
+    contact_detail = ContactDetail.new
+    assert_not contact_detail.save
+  end
+
+  test "should not save contact_detail without email" do
+    contact_detail = ContactDetail.new
+    assert_not contact_detail.save
+  end
+
+  test "should not save contact_detail without message_content" do
+    contact_detail = ContactDetail.new
+    assert_not contact_detail.save
+  end
 end
