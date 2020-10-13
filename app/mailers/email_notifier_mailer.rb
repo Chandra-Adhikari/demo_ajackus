ab873f5535990e7ce279f6764f880b4945deee0c
Chandra Adhikari <chandra.adhikari1994@gmail.com>
2020-10-14 02:26:52 +0530

    Change default target email to trigger email

--- app/mailers/email_notifier_mailer.rb
+++ app/mailers/email_notifier_mailer.rb
@@ -2,6 +2,6 @@ class EmailNotifierMailer < ApplicationMailer
 
   def contact_us_notifier(contact_user)
     @user = contact_user
-    mail(to: 'chandra.adhikari1994@gmail.com', subject: I18n.t('mailer.subject'))
+    mail(to: 'info@ajackus.com', subject: I18n.t('mailer.subject'))
   end
 end
